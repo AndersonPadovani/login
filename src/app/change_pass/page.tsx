35279f400a72e127e583c8a36f8d0f2e867dd69b
AndersonPadovani <andersonpadovani@protonmail.com>
2024-10-16 21:10:11 -0300

    Create page ChangePass

--- src/app/change_pass/page.tsx
+++ src/app/change_pass/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function ChangePass() {
   const urlParms = useSearchParams();
   const forgoutToken = urlParms.get("forgoutToken");
 
   return (
-    <main className="w-full h-full">change pass page {forgoutToken} </main>
+    <Suspense>
+      <main className="w-full h-full">change pass page {forgoutToken} </main>
+    </Suspense>
   );
 }
