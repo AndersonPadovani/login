4c85e0b21cbaa80694c3beb079f47e0c95b50e89
AndersonPadovani <andersonpadovani@protonmail.com>
2024-10-16 21:22:44 -0300

    Create page ChangePass

--- src/app/change_pass/page.tsx
+++ src/app/change_pass/page.tsx
@@ -3,12 +3,19 @@ import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function ChangePass() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="w-full h-full">
+        change pass page
+        <ForgoutToken />
+      </main>
+    </Suspense>
+  );
+}
+
+function ForgoutToken() {
   const urlParms = useSearchParams();
   const forgoutToken = urlParms.get("forgoutToken");
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <main className="w-full h-full">change pass page {forgoutToken} </main>
-    </Suspense>
-  );
+  return <>{forgoutToken}</>;
 }
